0f4869888a76a5436ce67ab7caf0416115242739
peberino <106129284+peberino@users.noreply.github.com>
2022-06-23 23:48:44 -0700

    Shrinked code

--- app.js
+++ app.js
@@ -4,16 +4,7 @@ let previousOperator;
 
 // Buttons
 const result = document.querySelector("#result");
-const zero = document.querySelector("#zero");
-const one = document.querySelector("#one");
-const two = document.querySelector("#two");
-const three = document.querySelector("#three");
-const four = document.querySelector("#four");
-const five = document.querySelector("#five");
-const six = document.querySelector("#six");
-const seven = document.querySelector("#seven");
-const eight = document.querySelector("#eight");
-const nine = document.querySelector("#nine");
+const numbers = document.querySelectorAll(".calcnum");
 const clear = document.querySelector("#clear");
 const backspace = document.querySelector("#backspace");
 const division = document.querySelector("#division");
@@ -32,16 +23,9 @@ const onNumberClick = (num) => () => {
   }
 };
 
-zero.addEventListener("click", onNumberClick(0));
-one.addEventListener("click", onNumberClick(1));
-two.addEventListener("click", onNumberClick(2));
-three.addEventListener("click", onNumberClick(3));
-four.addEventListener("click", onNumberClick(4));
-five.addEventListener("click", onNumberClick(5));
-six.addEventListener("click", onNumberClick(6));
-seven.addEventListener("click", onNumberClick(7));
-eight.addEventListener("click", onNumberClick(8));
-nine.addEventListener("click", onNumberClick(9));
+for (let num of numbers) {
+  num.addEventListener("click", onNumberClick(Number(num.innerText)));
+}
 
 clear.addEventListener("click", () => {
   result.innerText = "0";
@@ -57,27 +41,16 @@ backspace.addEventListener("click", () => {
 });
 
 // Operators
-const prevOp = (operator) => {
+const prevOp = (operator) => () => {
   runningTotal = result.innerText;
   previousOperator = `${operator}`;
   result.innerText = "0";
 };
 
-division.addEventListener("click", () => {
-  prevOp(`/`);
-});
-
-multiplication.addEventListener("click", () => {
-  prevOp("*");
-});
-
-minus.addEventListener("click", () => {
-  prevOp("-");
-});
-
-plus.addEventListener("click", () => {
-  prevOp("+");
-});
+division.addEventListener("click", prevOp(`/`));
+multiplication.addEventListener("click", prevOp("*"));
+minus.addEventListener("click", prevOp("-"));
+plus.addEventListener("click", prevOp("+"));
 
 equals.addEventListener("click", () => {
   runningTotal = `${runningTotal} ${previousOperator} ${result.innerText}`;
